a1089569f4ea7dd317bc1927a58a627dd14c9d28
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2024-02-21 00:16:29 +0200

    set up express backend

--- api/server.js
+++ api/server.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((error) => {
+    console.log("Error in DB connection", error);
+  });
 
 const app = express();
 
